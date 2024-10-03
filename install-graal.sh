97c6c86e2843f369ed1e73e807be9b478b3fc9ca
bart.charbon <b.charbon@umcg.nl>
2024-10-03 14:56:43 +0200

    fix graal caching mechanism

--- install-graal.sh
+++ install-graal.sh
@@ -5,7 +5,7 @@ if [ -z "$HOME" ]; then
   echo "Missing home dir parameter";
   exit 1;
 else
-  if [ ! -d "$HOME/graalvm/graalvm-community-openjdk-21.0.2+13.1" ]; then
+  if [ ! -d "$HOME/graalvm/graalvm" ]; then
     echo "GraalVM is not cached, installing...";
     wget https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-21.0.2/graalvm-community-jdk-21.0.2_linux-x64_bin.tar.gz;
     mkdir -p $HOME/graalvm;
