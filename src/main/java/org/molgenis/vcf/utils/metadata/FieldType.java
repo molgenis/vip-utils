591e73baea73646ad66e454d5ad4af6667c7bc0c
bart.charbon <b.charbon@umcg.nl>
2024-11-26 10:53:16 +0100

    Cleanup FieldType for actual used values in this repo

--- src/main/java/org/molgenis/vcf/utils/metadata/FieldType.java
+++ src/main/java/org/molgenis/vcf/utils/metadata/FieldType.java
@@ -1,21 +1,6 @@
 package org.molgenis.vcf.utils.metadata;
 
 public enum FieldType {
-  COMMON,
   INFO,
-  /**
-   * INFO field with nested information
-   */
-  INFO_NESTED,
   FORMAT,
-  /**
-   * FORMAT field with nested information (GENOTYPE info from htsjdk)
-   *
-   */
-  GENOTYPE,
-  /**
-   * Sample information
-   *
-   */
-  SAMPLE
 }
